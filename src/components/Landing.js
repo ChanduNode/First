1aca20d879e4687f56aa84119461ffadb5acc444
ChanduNode <79005593+ChanduNode@users.noreply.github.com>
2022-10-18 12:10:22 +0530

    Update Landing.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,6 +7,10 @@ class Landing extends Component {
         <div className="jumbotron mt-5">
           <div className="col-sm-8 mx-auto">
             <h1 className="text-center">WELCOME to react...</h1>
+      <h4> EC2 test passed</h4>
+      <h4> CI test passed</h4>
+       <h4> CD test passed</h4>
+       
           </div>
         </div>
       </div>
